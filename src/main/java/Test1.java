fb3f3a7ad6d052f698751d3aaa189e1152a40da7
PauloPortofolio <pvba02@gmail.com>
2022-09-28 13:16:08 -0400

    1.0) Java 17 Certif-Exam-Prep

---
+++ src/main/java/Test1.java
@@ -0,0 +1,14 @@
+
+
+public class Test1 {
+  public static void main(String[] args) {
+
+    var hasParams = (args == null ? false : true);
+    if (hasParams) {
+      System.out.println("has params");
+    }
+    {
+      System.out.println("no params");
+    }
+  }
+}
