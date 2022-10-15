4013a3cf8406228b1a0d882cfcf3faf7ebfc3432
PauloPortofolio <pvba02@gmail.com>
2022-10-15 16:40:05 -0400

    5.0) Java 17 Certif-Exam-Prep  - Test Switch

---
+++ src/main/java/SwitchTestLabels.java
@@ -0,0 +1,38 @@
+import java.time.LocalDate;
+
+import static java.time.DayOfWeek.*;
+
+public class SwitchTestLabels {
+  public static void main(String[] args) {
+
+    var day = LocalDate.now()
+                       .with(SUNDAY)
+//                       .with(FRIDAY)
+//                       .with(MONDAY)
+//                       .with(SATURDAY)
+                       .getDayOfWeek();
+
+    System.out.println(day);
+
+    switch (day) {
+      case MONDAY:
+        TUESDAY:
+        WEDNESDAY:
+        THURSDAY:
+        FRIDAY: System.out.println("working");
+      case SATURDAY:
+        SUNDAY:
+        System.out.println("off");
+    }
+
+//    switch (day) {
+//      case MONDAY:
+//        TUESDAY:
+//        WEDNESDAY:
+//        THURSDAY: System.out.println("working");
+//      case FRIDAY: System.out.println("friday");
+//      case SATURDAY:
+//        SUNDAY: System.out.println("off");
+//    }
+  }
+}
