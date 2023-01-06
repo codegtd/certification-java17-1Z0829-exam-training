48139b7b2394a2fdf2746864c7549c1327c85941
PauloPortofolio <pvba02@gmail.com>
2023-01-06 15:34:31 -0500

    7.0) Java 17 Certif-Exam-Prep  - Test Switch with multiple statements using ArrowKey  - Test Switch Scope

---
+++ src/main/java/SwitchScopeTest.java
@@ -0,0 +1,17 @@
+public class SwitchScopeTest {
+  public static void main(String[] args) {
+
+    switch (Integer.parseInt(args[1])) {
+      case 0:
+        var a = 2;
+        var b = false;
+        break;
+      case 1:
+//        int a = 3; // Compilation-Error
+        b = true;
+        break;
+    }
+
+//    if (b) System.out.println(args[2]); // Compilation-Error
+  }
+}
---
+++ src/main/java/SwitchTestsBraches.java
@@ -0,0 +1,37 @@
+import java.time.LocalDate;
+
+import static java.time.DayOfWeek.SUNDAY;
+
+public class SwitchTestsBraches {
+  public static void main(String[] args) {
+
+    var day = LocalDate.now()
+                       .with(SUNDAY)
+                       //                       .with(FRIDAY)
+                       //                       .with(MONDAY)
+                       //                       .with(SATURDAY)
+                       .getDayOfWeek();
+
+    System.out.println(
+         switch (day) {
+           case MONDAY, FRIDAY, SUNDAY -> 6;
+           case TUESDAY -> 7;
+           case THURSDAY, SATURDAY -> 8;
+           case WEDNESDAY -> 9;
+           default -> throw new IllegalStateException("Invalid day: " + day);
+         }
+    );
+
+
+    switch (day) {
+      case FRIDAY, SUNDAY -> {
+        System.out.println("mult-stat 1");
+        System.out.println("mult-stat 2");
+      }
+      default -> throw new
+        IllegalStateException("Invalid" + day);
+    }
+
+
+  }
+}
